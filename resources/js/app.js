7c3c9c4ff46d2363b272542aa66208df3b54243e
Lakshan Madushanka <epmadushanka@gmail.com>
2025-09-03 12:00:47 +0530

    dep (npm): primevue

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,10 @@ import './bootstrap';
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+
 createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
@@ -11,6 +15,11 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+            })
             .mount(el)
     },
 })

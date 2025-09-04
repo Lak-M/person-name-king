95e83d4af91d1a7dcf12c6e17726bd4b5f5ed326
Lakshan Madushanka <epmadushanka@gmail.com>
2025-09-04 18:01:23 +0530

    remove : csrf protection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,19 +6,25 @@ use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Validation\ValidationException;
 use Lakm\PersonName\Exceptions\InvalidNameException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [
-            HandleInertiaRequests::class,
-        ]);
+        $middleware->web(
+            append: [
+                HandleInertiaRequests::class,
+            ],
+            remove: [
+               VerifyCsrfToken::class,
+            ]
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (InvalidNameException $exception): void {

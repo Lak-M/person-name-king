b70ec1cffad4f29b1992cb2de8134ac7809b97dc
Lakshan Madushanka <epmadushanka@gmail.com>
2025-09-04 16:52:36 +0530

    feat : client side

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,7 @@ declare(strict_types=1);
 use App\Http\Controllers\FormatController;
 use Illuminate\Support\Facades\Route;
 
+Route::inertia('/', 'Home');
+
 Route::post('full-name/format', [FormatController::class, 'formatFullName'])->name('format-full-name');
 Route::post('name-parts/format', [FormatController::class, 'formatNameParts'])->name('format-name-parts');

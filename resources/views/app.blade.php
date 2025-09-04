288c6832863774b7f3923e0ae00ea019d3ce48b2
Lakshan Madushanka <epmadushanka@gmail.com>
2025-09-04 16:46:15 +0530

    feat : tailwind styles

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
-    @vite('resources/js/app.js')
+    @vite(['resources/css/app.css', 'resources/ts/app.js'])
     @inertiaHead
 </head>
 <body>

9fc09536d3b3091670e6d2890c47d77246512289
Lakshan Madushanka <epmadushanka@gmail.com>
2025-09-04 17:46:34 +0530

    chore

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/ts/app.ts'],
+            input: ['resources/css/app.css', 'resources/ts/app.js'],
             refresh: true,
         }),
         tailwindcss(),
